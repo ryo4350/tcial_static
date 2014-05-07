f610556c1ec0d2ead3c1ca8dc77fc5e0f7502d8f
諒 <@ryo>
2014-05-07 15:53:50 +0900

    細かな修正

--- wp-content/themes/cunningham/header.php
+++ wp-content/themes/cunningham/header.php
@@ -18,6 +18,17 @@
 <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
 <link href='http://fonts.googleapis.com/css?family=Lily+Script+One' rel='stylesheet' type='text/css'>
 <?php wp_head(); ?>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-41472504-1', 'tcial.jp');
+  ga('send', 'pageview');
+
+</script>
+</head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
@@ -25,6 +36,7 @@
             <div id="header-magazine">
                 <div class="container">
                     <h1 id="logo"><a href="<?php echo home_url('/magazine'); ?>"><?php bloginfo('name'); ?></a></h1>
+                    <h2 id="top-description" class="top-desc-magazine"><a href="/" target="_blank">個別指導ティーシャル</a>が運営するウェブマガジン</h2>
                 </div>
             </div>
 
@@ -39,7 +51,7 @@
             <?php wp_nav_menu( array( 'theme_location' => 'primary_magazine' ) ); ?>
             <ul class="sns-contact">
                 <li><a class="btn-flat-icon" id="fb-icon" href="https://www.facebook.com/tcialcom" target="_blank">f</a></li>
-                <li><a class="btn-flat-icon" id="tw-icon" href="/twitter" target="_blank">t</a></li>
+                <li><a class="btn-flat-icon" id="tw-icon" href="https://twitter.com/Tcial315" target="_blank">t</a></li>
             </ul>
 	</nav>
 
